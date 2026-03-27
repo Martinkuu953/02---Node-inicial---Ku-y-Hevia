e64da2dd4e4cfa92421577023c35ace417a67293
Max <95097703@est.ort.edu.ar>
2026-03-27 10:22:22 -0300

    Update ej01.js

--- src/modules/ej01.js
+++ src/modules/ej01.js
@@ -4,7 +4,8 @@ let resultado;
 
 function concatenarEInvertir(string1, string2) {
     let string3 = string2 + string1;
-    return string3;
+    let inversion = string3.split("").reverse().join("");
+    return inversion;
 }
 
 console.log(concatenarEInvertir("Establecimiento", "Educativo"));
