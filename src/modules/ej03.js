35fcf0cc8b6d2942a0815d57ca048875a8aa5656
Max <95097703@est.ort.edu.ar>
2026-03-27 09:53:24 -0300

    Update ej03.js

--- src/modules/ej03.js
+++ src/modules/ej03.js
@@ -1,11 +1,11 @@
 import Alumno from '../models/alumno.js';
 
-let alumno1 = new Alumno("Esteban Dido"  , "22888444", 20);
-let alumno2 = new Alumno("Matias Queroso", "28946255", 51);
+let alumno1 = new Alumno("Maximo Hevia"  , "23464244", 12);
+let alumno2 = new Alumno("Martín Ku", "13513553", 53);
 let alumno3 = new Alumno();
 
-alumno3.username    = "Elba Calao";
-alumno3.dni         = "32623391";
+alumno3.username = "Ernesto Longo";
+alumno3.dni = "23472421";
 
 console.clear();
 console.log(alumno1);
